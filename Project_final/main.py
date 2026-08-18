9ba6cc8e30499651be86ee94a1cd15634c4e63e3
chiamara02 <chiaramarangoni02@gmail.com>
2026-08-18 16:23:19 +0200

    first version of cmac; test scripts

---
+++ Project_final/main.py
@@ -0,0 +1,18 @@
+
+def main():
+
+    # initialize cmac, cv algo, neural network
+
+    # collect first 3 positions to initialize sliding window
+
+    # loop:
+    #   predict next position
+    #   wait for true next position to become available
+    #   compute error and update cmac weights
+
+    # every 10 seconds (20 steps)
+        # predict cmac
+        # give output to nn
+        # throw ball with output of nn
+        # measure error of throw (just for fun)
+        pass
